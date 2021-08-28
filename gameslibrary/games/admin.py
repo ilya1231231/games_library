bd3ecbab6747da4d44840449c3d06af6b4e814ac
Ilya Malygin <malygin.ilja@yandex.ru>
2021-08-28 17:25:58 +0300

    added images for admin panel

--- gameslibrary/games/admin.py
+++ gameslibrary/games/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from .models import (
     Category,
     Company,
@@ -12,10 +14,22 @@ from .models import (
 )
 
 
-class ReviewInLine(admin.StackedInline):  # Класс для отображения отзыва в игре
+class ReviewInLine(admin.TabularInline):  # Класс для отображения отзыва в игре
     model = Review
     extra = 1  # Указываю количество пустых полей
     readonly_fields = ('name', 'email')
+
+
+class GameScreenShootsInLine(admin.TabularInline):  # Отображение скриншотов в дитейле игры
+    model = GameScreenShoots
+    extra = 1
+    readonly_fields = ('get_image',)
+
+    def get_image(self, obj):
+        return mark_safe(
+            f'<img src={obj.image.url} width="100" height="auto"')  # mark_safe с f строкой для отображения в админке
+
+    get_image.short_description = 'Изображение'
 
 
 @admin.register(Category)
@@ -29,29 +43,36 @@ class GameAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url', 'draft')
     list_filter = ('category', 'year')  # Фильтрация по полю
     search_fields = ('title',)  # Поиск по названию игры
-    inlines = [ReviewInLine]  # Добавляю класс для отображения отзыва во вкладке игры
+    inlines = [GameScreenShootsInLine, ReviewInLine]  # Добавляю класс для отображения отзыва,скриншота во вкладке игры
     save_on_top = True  # Кнопки сохранения и удаления оставляем наверху
     save_as = True  # Функция сохранения как нового объекта
-    list_editable = ('draft', )  # Добавил чекбокс для удобного сохранения в черновик
+    list_editable = ('draft',)  # Добавил чекбокс для удобного сохранения в черновик
+    readonly_fields = ('get_image',)
     '''Группировка полей для удобной работы'''
     fieldsets = (
         (None, {
-            'fields': (('title', ), )
+            'fields': (('title',),)
         }),
         ('Описание и постер игры', {
-            'fields': (('description', 'poster'), )
+            'fields': (('description', 'get_image'),)
         }),
         ('Даты выпуска', {
-            'fields': (('year', 'country', 'release_date'), )
+            'fields': (('year', 'country', 'release_date'),)
         }),
         ('Компании, игровые платформы, жанры, категории', {
-            'classes': ('collapse', ),  # Функция для сокращения столбца
-            'fields': (('company', 'game_platform', 'genre', 'category'), )
+            'classes': ('collapse',),  # Функция для сокращения столбца
+            'fields': (('company', 'game_platform', 'genre', 'category'),)
         }),
         ('Опции', {
-            'fields': (('url', 'draft'), )
+            'fields': (('url', 'draft'),)
         }),
     )
+
+    def get_image(self, obj):
+        return mark_safe(
+            f'<img src={obj.poster.url} width="150" height="auto"')  # mark_safe с f строкой для отображения в админке
+
+    get_image.short_description = 'Постер игры'
 
 
 @admin.register(Review)
@@ -62,7 +83,14 @@ class ReviewAdmin(admin.ModelAdmin):
 
 @admin.register(Company)
 class CompanyAdmin(admin.ModelAdmin):
-    list_display = ('name', 'year')
+    list_display = ('name', 'year', 'get_image')  # Имя метода указанного ниже
+    readonly_fields = ('get_image',)  # Для отображения в дитейле при открытии
+
+    def get_image(self, obj):
+        return mark_safe(
+            f'<img src={obj.image.url} width="100" height="auto"')  # mark_safe с f строкой для отображения в админке
+
+    get_image.short_description = 'Изображение'
 
 
 @admin.register(Genre)
@@ -77,7 +105,14 @@ class GamePlatformAdmin(admin.ModelAdmin):
 
 @admin.register(GameScreenShoots)
 class GameScreenShootsAdmin(admin.ModelAdmin):
-    list_display = ('title', 'game')
+    list_display = ('title', 'game', 'get_image')
+    readonly_fields = ('get_image',)  # Для отображения в дитейле при открытии
+
+    def get_image(self, obj):
+        return mark_safe(
+            f'<img src={obj.image.url} width="70" height="auto"')  # mark_safe с f строкой для отображения в админке
+
+    get_image.short_description = 'Изображение'
 
 
 @admin.register(RatingStar)
@@ -89,3 +124,6 @@ class RatingStarAdmin(admin.ModelAdmin):
 class RatingAdmin(admin.ModelAdmin):
     list_display = ('ip', 'star', 'game')
 
+
+admin.site.site_title = 'Игровая библиотека'  # Меняем название
+admin.site.site_header = 'Игровая библиотека'
